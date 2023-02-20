2d09ef45a2c4bccf98785e6fddd909ce38dfda77
Mihai Idu <idumihai16@gmail.com>
2023-02-20 19:00:22 +0100

    Update mirroring_script.sh

--- registry/config/mirroring_script.sh
+++ registry/config/mirroring_script.sh
@@ -5,6 +5,6 @@ export LOCAL_REPO="ocp-release"
 export PULLSECRET_FILE=$1
 # we are going to use the localhost and port 5000 because this mirror will happen inside the registry container
 export LOCAL_REG="$2:5051"
-export OCP_VERSION=$4
+export OCP_VERSION=$3
 export UPSTREAM_REPO=$(curl -s https://mirror.openshift.com/pub/openshift-v4/clients/ocp/$OCP_VERSION/release.txt | grep 'Pull From: quay.io' | awk -F ' ' '{print $3}')
 oc adm release mirror -a ${PULLSECRET_FILE} --from=$UPSTREAM_REPO --to-release-image=$LOCAL_REG/$LOCAL_REPO:${VERSION} --to=$LOCAL_REG/$LOCAL_REPO --insecure=true
